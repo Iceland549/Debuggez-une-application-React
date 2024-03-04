9fafeae4ea4bd1f25c4544e3af8c62b1fb36b61e
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-03-04 12:13:20 +0100

    add const data and data.events.reduce to page function

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,8 @@ import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
-  const {last} = useData()
+  const {data} = useData()
+  const last = data ? data.events.reduce((acc, curr) => (acc.date > curr.date ? acc : curr), {}) : null;
   return <>
     <header>
       <Menu />
